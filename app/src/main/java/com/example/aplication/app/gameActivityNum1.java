b30aafbde1b00a3578846058e39c1f34ca9e962a
Pleshanov Kirill <k-paradox-p@mail.ru>
2017-02-25 00:37:44 +0300

    gradle update

--- app/src/main/java/com/example/aplication/app/gameActivityNum1.java
+++ app/src/main/java/com/example/aplication/app/gameActivityNum1.java
@@ -1,24 +1,13 @@
 package com.example.aplication.app;
 
-import android.app.Activity;
-import android.net.Uri;
-import android.os.Bundle;
-import android.os.StrictMode;
 import android.util.JsonReader;
 import android.util.Log;
-import android.webkit.WebView;
 import android.widget.TextView;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.String;import java.net.HttpURLConnection;
-import java.net.URL;
-import java.util.ArrayList;
 
-/**
- * Created by k-par_000 on 24.02.2017.
- */
+
 public class gameActivityNum1 extends AbstractGameActivity {
 
     private static final String TAG = "Game Activity #1";
@@ -59,4 +48,3 @@ public class gameActivityNum1 extends AbstractGameActivity {
 
     }
 }
-`
