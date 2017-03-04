0caacfa2fd5550010c0cb12e79df4910a4cf4a71
Nikita <botva96@mail.ru>
2017-03-05 01:30:57 +0300

    add mafia the best game beta version v1.04

---
+++ app/src/main/java/ru/ifmo/ctddev/application/games/MafiaGameActivity.java
@@ -0,0 +1,13 @@
+package ru.ifmo.ctddev.application.games;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MafiaGameActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_mafia_game);
+    }
+}
---
+++ app/src/main/java/ru/ifmo/ctddev/application/games/Player.java
@@ -0,0 +1,7 @@
+package ru.ifmo.ctddev.application.games;
+
+/**
+ * Created by User on 04.03.2017.
+ */
+public enum Player {
+}
